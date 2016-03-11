f18f7eb76a36f5b6fc2260c3f465e23931e84dfb
Axel Boersma <admin@ictabc.net>
2016-03-11 14:54:01 +0100

    Update to include domain pointers to exim and Dovecot SNI

--- scripts/update-mail-certificates.sh
+++ scripts/update-mail-certificates.sh
@@ -1,35 +1,47 @@
 #!/bin/sh
-
-#Change exim.conf
-#tls_certificate = ${if exists{/etc/exim.ssl/exim.cert.${tls_sni}}{/etc/exim.ssl/exim.cert.${tls_sni}}{/etc/exim.cert}}
-#tls_privatekey = ${if exists{/etc/exim.ssl/exim.key.${tls_sni}}{/etc/exim.ssl/exim.key.${tls_sni}}{/etc/exim.key}}
-
-#Change dovecot/conf/ssl.conf
-#!include /etc/dovecot/certs/*.conf
-
-#Also change it in custombuild so that after updates it still works.
-# Copy dovecot/conf/ssl.conf custombuild/custom/dovecot/conf/ (Create dir if needed)
-
 dir=/usr/local/directadmin/data/users/
 list=domains.list
 
 echo -n "Updating exim certificate...."
+cp /etc/exim.cert /etc/exim.cert.old
+cp /etc/exim.key /etc/exim.key.old
+cat /usr/local/directadmin/data/users/axel/domains/ictabc.net.cert > /etc/exim.cert
+cat /usr/local/directadmin/data/users/axel/domains/ictabc.net.cacert >> /etc/exim.cert
+cat /usr/local/directadmin/data/users/axel/domains/ictabc.net.key > /etc/exim.key
+
 for d in "$dir"*
 do
 	while read line
 	do	
 		if [ -f $d/domains/$line.cert ];
 		then
+			   echo $line
 			   cat $d/domains/$line.cert > /etc/exim.ssl/exim.cert.mail.$line
 			   cat $d/domains/$line.key > /etc/exim.ssl/exim.key.mail.$line
 			   cat $d/domains/$line.cacert >> /etc/exim.ssl/exim.cert.mail.$line
 			   cat $d/domains/$line.cert > /etc/exim.ssl/exim.cert.smtp.$line
 			   cat $d/domains/$line.key > /etc/exim.ssl/exim.key.smtp.$line
 			   cat $d/domains/$line.cacert >> /etc/exim.ssl/exim.cert.smtp.$line
+			if [ -f $d/domains/$line.pointers ];
+			then
+				cat $d/domains/$line.pointers
+				POINTERS=($(/bin/cat $d/domains/$line.pointers | /bin/sed "s/=alias$//"))
+	                          for y in ${POINTERS[@]}; do
+	                           cat $d/domains/$line.cert > /etc/exim.ssl/exim.cert.mail.$y
+       		                   cat $d/domains/$line.key > /etc/exim.ssl/exim.key.mail.$y
+       	        	           cat $d/domains/$line.cacert >> /etc/exim.ssl/exim.cert.mail.$y
+                        	   cat $d/domains/$line.cert > /etc/exim.ssl/exim.cert.smtp.$y
+  	                           cat $d/domains/$line.key > /etc/exim.ssl/exim.key.smtp.$y
+        	                   cat $d/domains/$line.cacert >> /etc/exim.ssl/exim.cert.smtp.$y
+				  done
+			fi
 		fi
 	done < $d/$list
 done
 
+#exim.conf
+#tls_certificate = ${if exists{/etc/exim.ssl/exim.cert.${tls_sni}}{/etc/exim.ssl/exim.cert.${tls_sni}}{/etc/exim.cert}}
+#tls_privatekey = ${if exists{/etc/exim.ssl/exim.key.${tls_sni}}{/etc/exim.ssl/exim.key.${tls_sni}}{/etc/exim.key}}
 
 
 echo "Done!"
@@ -97,8 +109,27 @@ do
 			   cp $d/domains/$line.key /etc/dovecot/certs/$line.key
 			   cp $d/domains/$line.cacert /etc/dovecot/certs/$line.cacert
 			   dovecot_ssl_intermediate $line
+  			   if [ -f $d/domains/$line.pointers ];
+			   then
+				POINTERS=($(/bin/cat $d/domains/$line.pointers | /bin/sed "s/=alias$//"))
+	                          for y in ${POINTERS[@]}; do
+			   		   cp $d/domains/$line.cert /etc/dovecot/certs/$y.cert
+					   cp $d/domains/$line.key /etc/dovecot/certs/$y.key
+					   cp $d/domains/$line.cacert /etc/dovecot/certs/$y.cacert
+					dovecot_ssl_intermediate $y
+				  done
+			   fi
 			else
+			   cp $d/domains/$line.cert /etc/dovecot/certs/$line.cert
+			   cp $d/domains/$line.key /etc/dovecot/certs/$line.key
 			   dovecot_ssl $line
+			   if [ -f $d/domains/$line.pointers ];
+			   then
+				POINTERS=($(/bin/cat $d/domains/$line.pointers | /bin/sed "s/=alias$//"))
+		                  for x in ${POINTERS[@]}; do
+					dovecot_ssl $x;
+				  done
+			   fi
 			fi	
 		fi
 	done < $d/$list
